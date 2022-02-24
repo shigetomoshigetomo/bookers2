122c0987a573ce9b72624c96ac85a467544c8d35
EC2 Default User <ec2-user@ip-172-31-33-12.ec2.internal>
2022-02-24 09:06:07 +0000

    [Add]edit.html update.destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    user=User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def index
     @users=User.all
     @book=Book.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profile_image)
   end
 end
