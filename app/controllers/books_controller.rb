6c759d5bbeb5e38ae929250356865738229e9d5e
EC2 Default User <ec2-user@ip-172-31-33-12.ec2.internal>
2022-02-26 10:42:12 +0000

    [Add] success_message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
     @book.user_id=current_user.id
     if @book.save
       redirect_to book_path(@book.id)
-      flash[:success]="You have created book successfully."
+      flash[:notice]="You have created book successfully."
     else
       render :index
     end
@@ -22,13 +22,16 @@ class BooksController < ApplicationController
 
   def edit
     @book=Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id)
-      flash[:success]="You have updated book successfully."
+      flash[:notice]="You have updated book successfully."
     else
       render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,21 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
     @books=@user.books
+    @book=Book.new
   end
 
   def edit
     @user=User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     @user=User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id)
-      flash[:success]="You have updated user successfully."
+      flash[:notice]="You have updated user successfully."
     else
       render :edit
     end
